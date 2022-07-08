ec419aa8e8d167d598b4303edd747ae4d107d6aa
Quillan Kaseman <q.kaseman@gmail.com>
2022-07-07 21:38:28 -0700

    feat(zsh): update completion settings

--- zsh/config.d/50-completion.zsh
+++ zsh/config.d/50-completion.zsh
@@ -1,2 +1,8 @@
-compinit -d ${ZSH_CACHE_HOME}/zcompdump-$ZSH_VERSION
 zstyle ':completion:*' cache-path ${ZSH_CACHE_HOME}/zcompcache
+zstyle ':completion:*' completer _complete _ignored _approximate
+zstyle ':completion:*' list-suffixes true
+zstyle ':completion:*' max-errors 1
+zstyle ':completion:*' squeeze-slashes true
+
+autoload -Uz compinit
+compinit -d ${ZSH_CACHE_HOME}/zcompdump-$ZSH_VERSION
