3f197c759a449f1e2f114f51120097a2fef5bca0
Quillan Kaseman <quillank@amazon.com>
2022-07-11 11:07:23 -0700

    feat(zsh): compinit -u
    May need to make this macOS only, not sure if I care though.

--- zsh/config.d/50-completion.zsh
+++ zsh/config.d/50-completion.zsh
@@ -5,4 +5,4 @@ zstyle ':completion:*' max-errors 1
 zstyle ':completion:*' squeeze-slashes true
 
 autoload -Uz compinit
-compinit -d ${ZSH_CACHE_HOME}/zcompdump-$ZSH_VERSION
+compinit -d ${ZSH_CACHE_HOME}/zcompdump-$ZSH_VERSION -u
