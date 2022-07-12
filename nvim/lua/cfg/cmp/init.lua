c83aa92757c34647ad1054fea27473e1d51b837a
Quillan Kaseman <quillank@amazon.com>
2022-07-12 11:14:20 -0700

    feat(vim): update completion keybindings

--- nvim/lua/cfg/cmp/init.lua
+++ nvim/lua/cfg/cmp/init.lua
@@ -13,11 +13,6 @@ if not ok then
   return
 end
 
-local has_words_before = function()
-  local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
-end
-
 -- Set completeopt to have a better completion experience
 vim.opt.completeopt = 'menuone,longest,preview'
 
@@ -52,41 +47,44 @@ cmp.setup({
   },
 
   mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
+    ["<C-k>"] = cmp.mapping.select_prev_item(),
+	["<C-j>"] = cmp.mapping.select_next_item(),
+    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+    ["<C-e>"] = cmp.mapping {
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    },
     ['<CR>'] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
       select = false,
     }),
-    ['<Tab>'] = cmp.mapping(
+    ["<Tab>"] = cmp.mapping(
       function(fallback)
-        -- if luasnip.expand_or_jumpable() then
-        --   luasnip.expand_or_jump()
-        if luasnip.jumpable(1) then
-          luasnip.jump(1)
-        elseif cmp.visible() then
+        if cmp.visible() then
           cmp.select_next_item()
-        elseif has_words_before() then
-          cmp.complete()
+        elseif luasnip.expandable() then
+          luasnip.expand()
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
         else
           fallback()
         end
       end,
-      { 'i', 's' }
+      { "i", "s", }
     ),
-    ['<S-Tab>'] = cmp.mapping(
+    ["<S-Tab>"] = cmp.mapping(
       function(fallback)
-        if luasnip.jumpable(-1) then
-          luasnip.jump(-1)
-        elseif cmp.visible() then
+        if cmp.visible() then
           cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+          luasnip.jump(-1)
         else
           fallback()
         end
       end,
-      { 'i', 's' }
+      { "i", "s", }
     ),
   }),
   sources = cmp.config.sources({
