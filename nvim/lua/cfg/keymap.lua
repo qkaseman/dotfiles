1866bb8f1319074988a3d182a9ef7cc15047fba5
Quillan Kaseman <quillank@amazon.com>
2022-07-12 13:59:12 -0700

    feat(vim): gj/gk <-> j/k

--- nvim/lua/cfg/keymap.lua
+++ nvim/lua/cfg/keymap.lua
@@ -11,3 +11,9 @@ local opts = { noremap=true, silent=true }
 
 -- Make semicolon into colon, because lazy.
 vim.keymap.set('', ';', ':', opts)
+
+-- Wrapped lines to go next by default.
+vim.keymap.set('n', 'j', 'gj', opts)
+vim.keymap.set('n', 'k', 'gk', opts)
+vim.keymap.set('n', 'gj', 'j', opts)
+vim.keymap.set('n', 'gk', 'k', opts)
