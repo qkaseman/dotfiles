0753fde81ff6f6e55cfa3c237200ac0eaee93cf3
Quillan Kaseman <quillank@amazon.com>
2022-07-20 12:53:40 -0700

    feat(zsh): add `fzf-tab-completion` submodule
    To make trying it and `fzf-tab` out a bit easier. Both seem to have
    their odd issues unfortunately.

--- zsh/config.d/60-fzf-tab.zsh
+++ zsh/config.d/60-fzf-tab.zsh
@@ -1,3 +1,7 @@
+# `fzf-tab-completion`, but these don't play well together.
+# source ${ZDOTDIR}/plugin/fzf-tab-completion/zsh/fzf-zsh-completion.sh
+# bindkey '^I' fzf_completion
+
 # This needs to be loaded after `compinit` but before any plugins that wrap
 # widgets, like `zsh-autosuggestions` or `fast-syntax-highlighting`.
 #
