d2a83c3ed845e5bc8f4b4d05d8e689fdda8c80d0
Quillan Kaseman <quillank@amazon.com>
2022-07-22 09:55:44 -0700

    fix(vim): update nvim-tree config
    Global options were moved to `setup`, which is nicer anyway.

--- nvim/lua/cfg/nvim-tree/init.lua
+++ nvim/lua/cfg/nvim-tree/init.lua
@@ -3,11 +3,12 @@ if not ok then
   return
 end
 
-vim.g.nvim_tree_highlight_opened_files = 1
-
 tree.setup({
   git = {
     ignore = false,
+  },
+  renderer = {
+    highlight_opened_files = "icon",
   },
 })
 
