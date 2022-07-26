d62719ff35c96a608f56648bf9b80811c88733d8
Quillan Kaseman <quillank@amazon.com>
2022-07-26 12:47:50 -0700

    refactor(vim): remove a tab

--- nvim/lua/cfg/cmp/init.lua
+++ nvim/lua/cfg/cmp/init.lua
@@ -48,7 +48,7 @@ cmp.setup({
 
   mapping = cmp.mapping.preset.insert({
     ["<C-k>"] = cmp.mapping.select_prev_item(),
-	["<C-j>"] = cmp.mapping.select_next_item(),
+    ["<C-j>"] = cmp.mapping.select_next_item(),
     ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
     ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
     ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
