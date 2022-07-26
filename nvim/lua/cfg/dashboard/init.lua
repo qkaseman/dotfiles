1e04457ab5c448c6058a34379ae36d036b03ae89
Quillan Kaseman <quillank@amazon.com>
2022-07-26 12:41:43 -0700

    feat(vim) add telescope to dashboard

--- nvim/lua/cfg/dashboard/init.lua
+++ nvim/lua/cfg/dashboard/init.lua
@@ -2,3 +2,10 @@ local ok, db = pcall(require, 'dashboard')
 if not ok then
   return
 end
+
+db.custom_center = {
+  { icon = '  ', desc = 'New file                            ', action = 'enew' }, -- longer desc b/c no shortcut
+  { icon = '  ', desc = 'Find file                      ', shortcut = 'SPC f', action = 'Telescope find_files' },
+  { icon = '  ', desc = 'Recent files                   ', shortcut = 'SPC h', action = 'Telescope oldfiles' },
+  { icon = '  ', desc = 'Find Word                      ', shortcut = 'SPC r', action = 'Telescope live_grep' },
+}
