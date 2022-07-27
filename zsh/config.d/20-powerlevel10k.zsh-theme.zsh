006dab723592fd4ae297e731bf06e6cb9d1991e7
Quillan Kaseman <quillank@amazon.com>
2022-07-27 11:52:40 -0700

    refactor(zsh): make powerlevel10k easier to reconfigure

---
+++ zsh/config.d/20-powerlevel10k.zsh-theme.zsh
@@ -0,0 +1,6 @@
+# This needs to come before the powerlevel10k configuration but the
+# configuration utility overwrites the entire file. That means you have to go
+# add this `source` back to the top of it every time, which is annoying.
+#
+# This avoids that by splitting them.
+source ${ZDOTDIR}/plugin/powerlevel10k/powerlevel10k.zsh-theme
