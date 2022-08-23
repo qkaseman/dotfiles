56a17bc7e26c306b0df0cc3f4ebeb98409a18bde
Quillan Kaseman <quillank@amazon.com>
2022-08-23 13:08:48 -0700

    feat(vim): update nvim-tree keymaps
    Removes most of the dumb defaults, adds some of my own that I expect to
    work.

--- nvim/lua/cfg/nvim-tree/init.lua
+++ nvim/lua/cfg/nvim-tree/init.lua
@@ -8,7 +8,45 @@ tree.setup({
     ignore = false,
   },
   renderer = {
-    highlight_opened_files = "icon",
+    highlight_opened_files = 'icon',
+  },
+  view = {
+    mappings = {
+      list = {
+        { -- Remove annoying default bindings.
+          key = {
+            '<2-LeftMouse>', '<2-RightMouse>', '<C-e>', 'O', '<C-]>', '<', '>',
+            '<Tab>', 'K', 'J', 'd', 'D', '<C-r>', 'x', 'c', 'p', '<C-v>',
+            '<C-x>', '<C-t>', '[e', '[c', ']e', ']c', 'y', 'Y', 'gy', 'f', 'F',
+            'S', '.', 'm', 'bmv',
+          },
+          action = ''
+        },
+        { key = { '<CR>', 'o' },                  action = 'edit' },
+        { key = 'v',                              action = 'vsplit' },
+        { key = 's',                              action = 'split' },
+        { key = 't',                              action = 'tabnew' },
+        { key = 'P',                              action = 'parent_node' },
+        { key = '<BS>',                           action = 'close_node' },
+        { key = 'I',                              action = 'toggle_git_ignored' },
+        { key = 'H',                              action = 'toggle_dotfiles' },
+        { key = 'U',                              action = 'toggle_custom' },
+        { key = 'R',                              action = 'refresh' },
+        { key = 'n',                              action = 'create' },
+        { key = 'r',                              action = 'rename' },
+        -- { key = 'y',                              action = 'copy_name' },
+        -- { key = 'Y',                              action = 'copy_path' },
+        -- { key = 'gy',                             action = 'copy_absolute_path' },
+        { key = '-',                              action = 'dir_up' },
+        -- { key = 'f',                              action = 'live_filter' },
+        -- { key = 'F',                              action = 'clear_live_filter' },
+        { key = 'q',                              action = 'close' },
+        { key = 'W',                              action = 'collapse_all' },
+        { key = 'E',                              action = 'expand_all' },
+        { key = '<C-k>',                          action = 'toggle_file_info' },
+        { key = 'g?',                             action = 'toggle_help' },
+      },
+    },
   },
 })
 
