089a10f924ef6f55337b72e00412d4ab90827bf2
Quillan Kaseman <quillank@amazon.com>
2022-08-23 13:07:17 -0700

    feat(vim): update completion keymaps

--- nvim/lua/cfg/cmp/init.lua
+++ nvim/lua/cfg/cmp/init.lua
@@ -47,8 +47,8 @@ cmp.setup({
   },
 
   mapping = cmp.mapping.preset.insert({
-    ["<C-k>"] = cmp.mapping.select_prev_item(),
-    ["<C-j>"] = cmp.mapping.select_next_item(),
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
     ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
     ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
     ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
@@ -56,18 +56,16 @@ cmp.setup({
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     },
-    ['<CR>'] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = false,
-    }),
+    -- ['<CR>'] = cmp.mapping.confirm({
+    --   behavior = cmp.ConfirmBehavior.Replace,
+    --   select = false,
+    -- }),
     ["<Tab>"] = cmp.mapping(
       function(fallback)
-        if cmp.visible() then
+        if luasnip.jumpable(1) then
+          luasnip.jump(1)
+        elseif cmp.visible() then
           cmp.select_next_item()
-        elseif luasnip.expandable() then
-          luasnip.expand()
-        elseif luasnip.expand_or_jumpable() then
-          luasnip.expand_or_jump()
         else
           fallback()
         end
@@ -76,10 +74,10 @@ cmp.setup({
     ),
     ["<S-Tab>"] = cmp.mapping(
       function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        elseif luasnip.jumpable(-1) then
+        if luasnip.jumpable(-1) then
           luasnip.jump(-1)
+        elseif cmp.visible() then
+          cmp.select_prev_item()
         else
           fallback()
         end
