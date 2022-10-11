ff24e14834512a56cbb8d6f71e3763e773098c81
Quillan Kaseman <quillank@amazon.com>
2022-10-11 10:32:25 -0700

    feat(vim): move to using `leader` rather than `space`

--- nvim/lua/cfg/lsp/on-attach.lua
+++ nvim/lua/cfg/lsp/on-attach.lua
@@ -1,24 +1,24 @@
 local setup_buffer_keymaps = function(bufnr)
   -- See `:help vim.lsp.*` for documentation on any of the below functions.
   local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<leader>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workleader_folders()))
   end, bufopts)
-  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+  vim.keymap.set('n', '<leader>l', vim.lsp.buf.formatting, bufopts)
 
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
   vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
   -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
   vim.keymap.set('n', 'gr', ':Telescope lsp_references<CR>', bufopts)
-  -- vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-  -- vim.keymap.set('v', '<space>ca', vim.lsp.buf.range_code_action, bufopts)
+  -- vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+  -- vim.keymap.set('v', '<leader>ca', vim.lsp.buf.range_code_action, bufopts)
   vim.keymap.set('n', '<leader>ca', ':Telescope lsp_code_actions<CR>', bufopts)
   vim.keymap.set('v', '<leader>ca', ':Telescope lsp_range_code_actions<CR>', bufopts)
   -- vim.keymap.set('n', '<leader>ca', ':CodeActionMenu<CR>', bufopts)
@@ -26,10 +26,10 @@ local setup_buffer_keymaps = function(bufnr)
 
   -- See `:help vim.diagnostic.*` for documentation on any of the below functions.
   local opts = { noremap=true, silent=true }
-  vim.keymap.set('n', '<space>d', vim.diagnostic.open_float, opts)
+  vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts)
   vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
   vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-  vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+  vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
 end
 
 return function(client, bufnr)
