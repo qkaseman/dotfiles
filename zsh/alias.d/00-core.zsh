089776c9d6e3d626e7b6871a786aa0099ff9029d
Quillan Kaseman <quillank@amazon.com>
2023-02-15 13:57:15 -0800

    feat(zsh): update core aliases
    - more `ls` aliases
    - better directory stack aliases

--- zsh/alias.d/00-core.zsh
+++ zsh/alias.d/00-core.zsh
@@ -7,7 +7,6 @@ alias ls="ls --color=auto"
 alias ..='cd ..'
 alias ...='cd ../../'
 alias -- -='cd -'
-alias l='ls -lah'
 alias usage='du -cksh'
 
 # Easy terminal reset.
@@ -16,3 +15,15 @@ alias cls='printf "\033c"'
 # Scratchpads.
 alias todo="vi ${XDG_DATA_HOME}/todo.md"
 alias scratch="vi ${XDG_DATA_HOME}/scratch.md"
+
+# Pretty dir stack
+alias dirs="dirs -v"
+alias d='dirs -v | head -10'
+
+# List directory contents.
+# -l - use long listing format
+# -a - all files, include dotfiles
+# -A - almost all, don't list '.' and '..'
+# -h - with '-l', human readable size formats
+alias l='ls -lah'
+alias la='ls -lAh'
