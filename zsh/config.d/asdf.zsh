c14652b65267aad8d09f0384a37e72f7404c01f7
Quillan Kaseman <quillank@amazon.com>
2023-03-21 16:59:01 -0700

    fix(zsh): asdf global install can't find tool-versions
    asdf changed their config file resolution so you can no longer force it
    to be XDG compliant. Symlink as a workaround but annoying.

--- zsh/config.d/asdf.zsh
+++ zsh/config.d/asdf.zsh
@@ -1,3 +1,11 @@
+# asdf is dumb and only allows `${HOME}/<filename>` level
+# of customization so symlink to pseudo-make compliant. See:
+# https://github.com/asdf-vm/asdf/blob/49e541a29ff7a2f35917a4544a8b9adbc02bb1b4/lib/functions/versions.bash#L23
+[ ! -f ${ASDF_GLOBAL_TOOL_VERSIONS} ] && \
+    touch ${ASDF_GLOBAL_TOOL_VERSIONS}
+[ ! -f ${ASDF_TOOL_VERSIONS_LINK} ] && \
+    ln -s ${ASDF_GLOBAL_TOOL_VERSIONS} ${ASDF_TOOL_VERSIONS_LINK}
+
 [ -d ${ASDF_BIN_HOME} ] && \
     source ${ASDF_BIN_HOME}/asdf.sh && \
     fpath+=(${ASDF_BIN_HOME}/completions)
--- zsh/env.d/asdf.zsh
+++ zsh/env.d/asdf.zsh
@@ -1,7 +1,12 @@
 # Make `asdf` follow XDG spec.
 export ASDF_DATA_DIR=${XDG_DATA_HOME}/asdf
 export ASDF_CONFIG_FILE=${XDG_CONFIG_HOME}/asdf/asdfrc
-export ASDF_DEFAULT_TOOL_VERSIONS_FILENAME=${ASDF_DATA_DIR}/tool-versions
 export ASDF_NPM_DEFAULT_PACKAGES_FILE=${ASDF_DATA_DIR}/npm/default-packages
 
 export ASDF_BIN_HOME=${XDG_BIN_HOME}/asdf
+
+# Oh yay, they broke support for this because they prepend `$HOME` and use this name
+# for local too... Intentionally... :|
+#export ASDF_DEFAULT_TOOL_VERSIONS_FILENAME=${ASDF_DATA_DIR}/tool-versions
+export ASDF_GLOBAL_TOOL_VERSIONS=${ASDF_DATA_DIR}/tool-versions
+export ASDF_TOOL_VERSIONS_LINK=${HOME}/.tool-versions
