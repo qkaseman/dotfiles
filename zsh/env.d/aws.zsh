7c7e57d46e085a92b496140bcece5419279f812f
Quillan Kaseman <quillank@amazon.com>
2023-03-22 10:12:07 -0700

    fix(zsh): update aws config to use STATE and CACHE
    Credentials and history files shouldn't be in DATA

--- zsh/env.d/aws.zsh
+++ zsh/env.d/aws.zsh
@@ -1,5 +1,5 @@
 # Make AWS CLI respect XDG spec as much as possible.
 export AWS_CONFIG_FILE=${XDG_CONFIG_HOME}/aws/config
-export AWS_CLI_HISTORY_FILE=${XDG_DATA_HOME}/aws/history
-export AWS_WEB_IDENTITY_TOKEN_FILE=${XDG_DATA_HOME}/aws/token
-export AWS_SHARED_CREDENTIALS_FILE=${XDG_DATA_HOME}/aws/credentials
+export AWS_CLI_HISTORY_FILE=${XDG_STATE_HOME}/aws/history
+export AWS_WEB_IDENTITY_TOKEN_FILE=${XDG_CACHE_HOME}/aws/token
+export AWS_SHARED_CREDENTIALS_FILE=${XDG_CACHE_HOME}/aws/credentials
