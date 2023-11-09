af155dad3c160b16dad5ad2abf425454f7a3964c
Quillan Kaseman <quillank@amazon.com>
2023-11-09 11:14:22 -0800

    feat(zsh): add alias to open daily worklog

--- zsh/alias.d/note.zsh
+++ zsh/alias.d/note.zsh
@@ -10,7 +10,8 @@ function scratch {
 
 # Log work done.
 #
-# Streams all arguments into a date-stamped file for organization. Adds a newline after them all.
+# Streams all arguments into a date-stamped file for organization. Adds a
+# newline after them all.
 function log {
     local timestamp=$(date +%H:%M)
     local date_stamp=$(date +%Y-%m-%d)
@@ -22,3 +23,7 @@ function log {
     printf '%s: %s\n\n' ${timestamp} "$*" >> ${fname}
 }
 
+# Could store the `date ...` string in a variable and `eval` it, but not sure
+# that's the best idea plus I believe the variable would pollute the general
+# variable closure which isn't great either.
+alias worklog-open="vi ${WORKLOG_HOME}/$(date +%Y-%m-%d).md"
