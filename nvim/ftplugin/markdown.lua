5f08712cc67bc3a6ae1ad15f648d585ceae8ee39
Quillan Kaseman <quillank@amazon.com>
2024-03-11 15:01:28 -0700

    feat(nvim): update markdown to 2 space tabs and 80 char wrap

--- nvim/ftplugin/markdown.lua
+++ nvim/ftplugin/markdown.lua
@@ -1,7 +1,8 @@
 vim.opt_local.expandtab = true
-vim.opt_local.softtabstop = 4
-vim.opt_local.shiftwidth = 4
-vim.opt_local.tabstop = 4
+vim.opt_local.softtabstop = 2
+vim.opt_local.shiftwidth = 2
+vim.opt_local.tabstop = 2
+vim.opt_local.textwidth = 80
 vim.opt_local.formatoptions:append({ c = true, r = true, o = true, q = true })
 vim.opt_local.spell = true
 vim.opt_local.linebreak = true
