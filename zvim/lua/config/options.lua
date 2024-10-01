bc83f83a374fc829c1b3ae5cf9ac51da041dec0a
Quillan Kaseman <quillank@amazon.com>
2024-10-01 13:36:05 -0700

    feat(zvim): fix weird LazyVim defaults

--- zvim/lua/config/options.lua
+++ zvim/lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Fix some of LazyVim's weird defaults
+vim.opt.relativenumber = false
+vim.g.autoformat = false
+vim.opt.autowrite = false
+vim.opt.wrap = true
