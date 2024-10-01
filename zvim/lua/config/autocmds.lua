660b16e3e889595096ad0483986f113e3d544ce8
Quillan Kaseman <quillank@amazon.com>
2024-10-01 14:49:02 -0700

    feat(zvim): add autocmd for stripping whitespace

--- zvim/lua/config/autocmds.lua
+++ zvim/lua/config/autocmds.lua
@@ -1,3 +1,22 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local au = vim.api.nvim_create_autocmd
+
+local group = vim.api.nvim_create_augroup('whitespace', { clear = true })
+au({ 'BufRead', 'BufWrite' }, {
+  group = group,
+  callback = function ()
+    local ws_regex = "\\s\\+"
+
+    -- Only do if the buffer is modifiable
+    if not vim.bo.modifiable or vim.o.binary and vim.o.filetype == 'diff' then
+      return
+    end
+
+    local view = vim.fn.winsaveview()
+    vim.cmd("silent! keeppatterns %s/"..ws_regex.."$//e")
+    vim.fn.winrestview(view)
+  end,
+} )
