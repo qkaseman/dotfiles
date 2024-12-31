de223e2301e3a9e34dc12a68d13ead4113ec75fd
Quillan Kaseman <quillank@amazon.com>
2024-12-31 10:24:04 -0800

    feat(nvim): strip trailling whitespace on save

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -11,3 +11,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+--- Remove all trailing whitespace on save
+local TrimWhiteSpaceGrp = vim.api.nvim_create_augroup("TrimWhiteSpaceGrp", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePre", {
+  command = [[:%s/\s\+$//e]],
+  group = TrimWhiteSpaceGrp,
+})
