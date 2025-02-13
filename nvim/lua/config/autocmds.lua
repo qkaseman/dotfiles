3e9a601a631158f953cf703a10eccb7aa530ae30
Quillan Kaseman <quillank@amazon.com>
2025-02-12 16:29:25 -0800

    feat(nvim): add (commented out) filetype autocommand
    It is unclear if the plugin to detect indentation works how I want in
    all cases. If it doesn't, I'll need to add something like this to handle
    filetype indentation.

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -18,3 +18,22 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   command = [[:%s/\s\+$//e]],
   group = TrimWhiteSpaceGrp,
 })
+
+--- Indentation/Format Settings ---
+-- Needed because file type plugins that Neovim provides get loaded after
+-- options are set.
+--[[
+local IndentationGrp = vim.api.nvim_create_augroup('IndentationGrp', { clear = true })
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { '*.js', '*.ts', '*.tsx', '*.jsx' },
+  callback = function()
+    vim.opt_local.expandtab = true
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+    vim.opt_local.textwidth = 80
+    vim.opt_local.formatoptions:append({ c = true, r = true, o = true, q = true })
+  end,
+    group = IndentationGrp,
+})
+--]]
