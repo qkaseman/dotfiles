59d97fcc444c9f323f8dab19ad08c0895f811cab
Quillan Kaseman <quillank@amazon.com>
2025-02-17 11:54:39 -0800

    feat(nvim): disable format on save
    Made it a functio that's easy to add back but format on save is
    sometimes too annoying.

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -1,3 +1,23 @@
+--- Format the buffer on save
+--
+-- @param bufnr - buffer to format
+function format_on_save(bufnr)
+  -- Disable "format_on_save lsp_fallback" for languages that don't
+  -- have a well standardized coding style. You can add additional
+  -- languages here or re-enable it for the disabled ones.
+  local disable_filetypes = { c = true, cpp = true }
+  local lsp_format_opt
+  if disable_filetypes[vim.bo[bufnr].filetype] then
+    lsp_format_opt = 'never'
+  else
+    lsp_format_opt = 'fallback'
+  end
+  return {
+    timeout_ms = 500,
+    lsp_format = lsp_format_opt,
+  }
+end
+
 return { -- Autoformat
   'stevearc/conform.nvim',
   event = { 'BufWritePre' },
@@ -14,22 +34,6 @@ return { -- Autoformat
   },
   opts = {
     notify_on_error = false,
-    format_on_save = function(bufnr)
-      -- Disable "format_on_save lsp_fallback" for languages that don't
-      -- have a well standardized coding style. You can add additional
-      -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes = { c = true, cpp = true }
-      local lsp_format_opt
-      if disable_filetypes[vim.bo[bufnr].filetype] then
-        lsp_format_opt = 'never'
-      else
-        lsp_format_opt = 'fallback'
-      end
-      return {
-        timeout_ms = 500,
-        lsp_format = lsp_format_opt,
-      }
-    end,
     formatters_by_ft = {
       lua = { 'stylua' },
       -- Conform can also run multiple formatters sequentially
