0e735507621bf4ee8d385e0a1623b8d457401e08
Quillan Kaseman <quillank@amazon.com>
2025-02-18 14:56:17 -0800

    feat(zsh): add XDG configuration for Ruby tooling

--- zsh/env.d/gem.zsh
+++
@@ -1,2 +0,0 @@
-export GEM_HOME=${XDG_DATA_HOME}/gem
-export GEM_SPEC_CACHE=${XDG_CACHE_HOME}/gem
---
+++ zsh/env.d/ruby.zsh
@@ -0,0 +1,11 @@
+# IRB
+export IRBRC="$XDG_CONFIG_HOME/ruby/irb/irbrc"
+
+# Ruby Gem Bundler
+export BUNDLE_USER_CONFIG="${XDG_CONFIG_HOME}/ruby/bundle"
+export BUNDLE_USER_CACHE="${XDG_CACHE_HOME}/ruby/bundle"
+export BUNDLE_USER_PLUGIN="${XDG_DATA_HOME}/ruby/bundle"
+
+# Gems
+export GEM_HOME=${XDG_DATA_HOME}/gem
+export GEM_SPEC_CACHE=${XDG_CACHE_HOME}/gem
