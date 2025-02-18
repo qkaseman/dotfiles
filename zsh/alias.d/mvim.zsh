e49b176cda065426cc549c5d48509e769097338e
Quillan Kaseman <quillank@amazon.com>
2025-02-18 15:09:54 -0800

    feat(zsh): update neovim aliases

--- zsh/alias.d/mvim.zsh
+++
@@ -1,5 +0,0 @@
-# `mvim` for "my vim"
-# My old setup that's being replaced but still want to keep it around
-# for now.
-alias mvim='NVIM_APPNAME=mvim nvim'
-alias zvim='NVIM_APPNAME=zvim nvim'
---
+++ zsh/alias.d/neovim.zsh
@@ -0,0 +1,1 @@
+alias avim='NVIM_APPNAME=avim nvim'
