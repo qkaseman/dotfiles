23db157aa35cde602610aca41c8d0253a9b4e9c3
Quillan Kaseman <quillank@amazon.com>
2025-02-24 17:42:27 -0800

    feat(setup): only install if the programs don't exist already

--- .setup/macos.sh
+++ .setup/macos.sh
@@ -3,23 +3,33 @@
 CONFIG_DIR=${XDG_CONFIG_HOME:-${HOME}/.config}
 
 # Install Homebrew.
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+[[ -x "$(command -v brew)" ]] || \
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Unfortunately install mise for environment management.
-curl https://mise.run | MISE_INSTALL_PATH="${XDG_BIN_HOME:-${HOME}/.local/bin}/mise" sh
+[[ -x "$(command -v mise)" ]] || \
+    curl https://mise.run | MISE_INSTALL_PATH="${XDG_BIN_HOME:-${HOME}/.local/bin}/mise" sh
 
 # Install default version of Node, needed for Neovim plugins.
-mise use -g node@20
+[[ -x "$(command -v node)" ]] || mise use -g node@20
 
 # Install tools.
-brew install \
-    git \
-    fzf \
-    alacritty \
-    neovim \
-    ripgrep \
-    skhd \
-    yabai
+
+# git is special as MacOS provides a shim to trigger installation if you
+# haven't but that makes it harder to detect if it is installed.
+INSTALL="git"
+PROGS=(
+  "fzf"
+  "alacritty"
+  "ripgrep"
+  "skhd"
+  "yabai"
+)
+for p in ${PROGS[@]}; do
+    [[ -x "$(command -v ${p})" ]] || INSTALL="${INSTALL} ${p}"
+done
+
+brew install ${INSTALL}
 
 # Clone common config.
 git clone --recurse-submodules https://github.com/qkaseman/dotfiles.git ${CONFIG_DIR}
