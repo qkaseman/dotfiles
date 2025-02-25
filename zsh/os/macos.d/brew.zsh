7771e1464cbce2d3eca34f4b4117794691075852
Quillan Kaseman <quillank@amazon.com>
2025-02-24 18:43:18 -0800

    feat(zsh): add support for both MacOS architectures

--- zsh/os/macos.d/brew.zsh
+++ zsh/os/macos.d/brew.zsh
@@ -4,6 +4,21 @@ export ZSH_DISABLE_COMPFIX="true"
 # https://docs.brew.sh/Analytics
 export HOMEBREW_NO_ANALYTICS=1
 
-fpath+=("$(brew --prefix)/share/zsh/site-functions")
+case "$(uname -m)" in
+  arm64)
+    BREW_PREFIX="/opt/homebrew"
+    ;;
+  *) # x86_64-*)
+    BREW_PREFIX="/usr/local"
+    ;;
+esac
 
-path=('/usr/local/opt/curl/bin' $path)
+export HOMEBREW_PREFIX="${BREW_PREFIX:-/usr/local}"
+export HOMEBREW_CELLAR="${HOMEBREW_PREFIX}/Cellar";
+export HOMEBREW_REPOSITORY="${HOMEBREW_PREFIX}/Homebrew";
+
+export INFOPATH="${HOMEBREW_PREFIX}/usr/local/share/info:${INFOPATH:-}";
+[ -z "${MANPATH-}" ] || export MANPATH=":${MANPATH#:}";
+
+fpath+=("${HOMEBREW_PREFIX}/share/zsh/site-functions")
+path+=("${HOMEBREW_PREFIX}/bin" '/usr/local/opt/curl/bin')
